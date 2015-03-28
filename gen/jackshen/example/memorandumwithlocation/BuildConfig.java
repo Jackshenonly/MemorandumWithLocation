f8c6d976f2d41745e6f629ece21536ce2dc693dc
Jackshenonly <253170334@qq.com>
2015-03-28 15:03:57 +0800

    提交代码

---
+++ gen/jackshen/example/memorandumwithlocation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jackshen.example.memorandumwithlocation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
