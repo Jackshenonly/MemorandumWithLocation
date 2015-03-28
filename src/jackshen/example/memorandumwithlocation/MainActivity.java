c5597a62aaf21c43ba4d58512d05e86bf04cfd98
Jackshenonly <253170334@qq.com>
2015-03-29 01:28:11 +0800

    添加高德地图SDK

--- src/jackshen/example/memorandumwithlocation/MainActivity.java
+++ src/jackshen/example/memorandumwithlocation/MainActivity.java
@@ -5,13 +5,73 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 
+public class MainActivity extends Activity {
+	
+	private MapView mapView;
+	private AMap aMap;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mapView = (MapView)findViewById(R.id.map);
+		mapView.onCreate(savedInstanceState);
+		init();
 	}
+	
+
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+    }
+ 
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+ 
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+     
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+ 
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+
+
+
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -26,9 +86,13 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch(id)
+		{
+		case R.id.action_settings:return true;
+		case R.id.action_finish:finish();
+			
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
 }
